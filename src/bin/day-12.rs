305b1f802de8d23fa424d332571f35217a0c0494
triarupmac <tri-k.nguyen@arup.com>
2023-12-13 09:58:21 +1100

    add raw day-12

---
+++ src/bin/day-12.rs
@@ -0,0 +1,146 @@
+use aoc_2023_lib::main;
+
+use std::error::Error;
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+main! {
+    let input = include_str!("../../inputs/day-12.txt");
+    (part_1(input).unwrap(),part_2(input).unwrap())
+}
+
+fn part_1(input: &str) -> Result<usize> {
+    Ok(arrangements(input, 1))
+}
+
+fn part_2(input: &str) -> Result<usize> {
+    Ok(arrangements(input, 5))
+}
+
+fn arrangements(input: &str, repetitions: usize) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let mut springs_single_rep = Vec::new();
+            let mut springs = Vec::<Spring>::new();
+            let mut groups_single_rep = Vec::new();
+            let mut groups = Vec::<usize>::new();
+            let mut stack = Vec::new();
+            let mut cache = Vec::new();
+
+            let mut parts = line.split_ascii_whitespace();
+
+            springs_single_rep.extend(parts.next().unwrap().chars().map(|c| match c {
+                '#' => Spring::Broken,
+                '.' => Spring::Operational,
+                '?' => Spring::Unknown,
+                _ => panic!(),
+            }));
+
+            groups_single_rep.extend(
+                parts
+                    .next()
+                    .unwrap()
+                    .split(',')
+                    .map(|num| num.parse::<usize>().unwrap()),
+            );
+
+            springs.reserve((springs_single_rep.len() + 1 * repetitions) - 1);
+            groups.reserve(groups_single_rep.len() * repetitions);
+            for _ in 0..repetitions {
+                if springs.len() > 0 {
+                    springs.push(Spring::Unknown);
+                }
+                springs.extend(&springs_single_rep);
+                groups.extend(&groups_single_rep);
+            }
+
+            cache.resize((groups.len() - 1) * springs.len(), None);
+            stack.reserve(groups.len() - 1);
+
+            let mut count = 0;
+            let mut pos = 0;
+
+            loop {
+                let mut indent = String::new();
+                for _ in 0..stack.len() {
+                    indent.extend("  ".chars());
+                }
+                let len = groups[stack.len()];
+                let end = pos + len;
+
+                if end > springs.len() || (pos > 0 && springs[pos - 1] == Spring::Broken) {
+                    // There's a broken spring that's not included in a group, or we've gone past the end
+                    if let Some((x, y)) = stack.pop() {
+                        pos = x;
+                        cache[stack.len() * springs.len() + pos] = Some(count);
+                        count = y + count;
+                        pos += 1;
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (end < springs.len() && springs[end] == Spring::Broken)
+                    || springs[pos..end].iter().any(|&x| x == Spring::Operational)
+                {
+                    // Not a valid position
+                    pos += 1;
+                    continue;
+                }
+
+                if stack.len() == groups.len() - 1 {
+                    if springs[end..].iter().all(|&x| x != Spring::Broken) {
+                        count += 1;
+                    }
+                    pos += 1;
+                } else {
+                    if let Some(old) = cache[stack.len() * springs.len() + pos] {
+                        count += old;
+                        pos += 1;
+                    } else {
+                        stack.push((pos, count));
+                        count = 0;
+                        pos = end + 1;
+                    }
+                }
+            }
+
+            count
+        })
+        .sum()
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Spring {
+    Unknown,
+    Broken,
+    Operational,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_INPUT: &str = include_str!("../../inputs/day-12-test.txt");
+    #[test]
+    fn test_input() {
+        assert_eq!(part_1(TEST_INPUT).unwrap(), 21);
+        assert_eq!(part_2(TEST_INPUT).unwrap(), 525_152);
+    }
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1(include_str!("../../inputs/day-12.txt")).unwrap(),
+            7_843
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2(include_str!("../../inputs/day-12.txt")).unwrap(),
+            10_153_896_718_999
+        );
+    }
+}
