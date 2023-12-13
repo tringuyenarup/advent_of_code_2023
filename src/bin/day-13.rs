8fc6510910c410c7d426abb4cda5d02b1f2d07c0
triarupmac <tri-k.nguyen@arup.com>
2023-12-13 18:15:04 +1100

    add day-13

--- src/bin/day-13.rs
+++ src/bin/day-13.rs
@@ -1,6 +1,6 @@
 use aoc_2023_lib::main;
 
-use std::error::Error;
+use std::{error::Error, usize};
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 main! {
@@ -8,27 +8,78 @@ main! {
     (part_1(input).unwrap(),part_2(input).unwrap())
 }
 
-fn part_1(input: &str) -> Result<i32> {
-    todo!()
+fn part_1(input: &str) -> Result<usize> {
+    let grids = input
+        .split("\n\n")
+        .map(|s| s.split('\n').map(|l| l.as_bytes()).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    Ok(solve(&grids, 0))
 }
 
-fn part_2(input: &str) -> Result<i32> {
-    todo!()
+fn part_2(input: &str) -> Result<usize> {
+    let grids = input
+        .split("\n\n")
+        .map(|s| s.split('\n').map(|l| l.as_bytes()).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    Ok(solve(&grids, 1))
+}
+fn find_col(grid: &[&[u8]], limit: usize) -> Option<usize> {
+    (0..grid[0].len() - 1).find(|&c| {
+        let incorrect = (0..=c.min(grid[0].len() - c - 2))
+            .map(|dc| {
+                let a = c - dc;
+                let b = c + 1 + dc;
+                (0..grid.len())
+                    .filter(|&r| grid[r][a] != grid[r][b])
+                    .count()
+            })
+            .sum::<usize>();
+        incorrect == limit
+    })
 }
 
+fn find_row(grid: &[&[u8]], limit: usize) -> Option<usize> {
+    (0..grid.len() - 1).find(|&r| {
+        let incorrect = (0..=r.min(grid.len() - r - 2))
+            .map(|dr| {
+                let a = r - dr;
+                let b = r + 1 + dr;
+                (0..grid[0].len())
+                    .filter(|&c| grid[a][c] != grid[b][c])
+                    .count()
+            })
+            .sum::<usize>();
+        incorrect == limit
+    })
+}
+
+fn solve(grids: &[Vec<&[u8]>], limit: usize) -> usize {
+    grids
+        .iter()
+        .map(|grid| {
+            find_row(grid, limit)
+                .map(|r| (r + 1) * 100)
+                .or_else(|| find_col(grid, limit).map(|c| c + 1))
+                .unwrap()
+        })
+        .sum()
+}
 #[cfg(test)]
 mod tests {
     use super::*;
     const TEST_INPUT: &str = include_str!("../../inputs/day-13-test.txt");
     #[test]
     fn test_input() {
-        assert_eq!(part_1(TEST_INPUT).unwrap(), 1);
-        // assert_eq!(part_2(TEST_INPUT).unwrap(), 1);
+        assert_eq!(part_1(TEST_INPUT).unwrap(), 405);
+        assert_eq!(part_2(TEST_INPUT).unwrap(), 400);
     }
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(include_str!("../../inputs/day-13.txt")).unwrap(), 1);
+        assert_eq!(
+            part_1(include_str!("../../inputs/day-13.txt")).unwrap(),
+            39_939
+        );
     }
 
     #[test]
