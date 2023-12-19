fab422a3ac477ea8edd5c77bd1925675a60f50a5
triarupmac <tri-k.nguyen@arup.com>
2023-12-19 11:02:30 +1100

    udpate day-18

--- src/bin/day-18.rs
+++ src/bin/day-18.rs
@@ -1,7 +1,6 @@
 use aoc_2023_lib::main;
-use itertools::Itertools;
 
-use std::{error::Error, str::FromStr};
+use std::error::Error;
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 main! {
@@ -9,112 +8,60 @@ main! {
     (part_1(input).unwrap(),part_2(input).unwrap())
 }
 
-fn part_1(input: &str) -> Result<i32> {
-    let plan = input.parse::<Plan>()?;
-    let (mut r, mut c, mut point) = (0, 0, 0);
-
-    let mut points = vec![(0, 0)];
-
-    for (direction, offset, _) in plan.digs {
-        match direction {
-            'U' => {
-                r -= offset;
-            }
-            'D' => {
-                r += offset;
-            }
-            'L' => {
-                c -= offset;
-            }
-            'R' => {
-                c += offset;
-            }
-            _ => panic!("ERROR: bad direction {direction}"),
-        }
-        points.push((r, c));
-        point += offset;
-    }
-    let a = points
-        .iter()
-        .tuple_windows()
-        .map(|(p1, p2)| (p1.1 + p2.1) * (p2.0 - p1.0))
-        .sum::<i32>();
-    Ok((point + a) / 2 + 1)
+fn part_1(input: &str) -> Result<isize> {
+    let dig_plans = parse(input, false);
+    shoelace_algorithm(dig_plans)
 }
 
 fn part_2(input: &str) -> Result<isize> {
-    let plan = input.parse::<Plan>()?;
-    let (mut r, mut c, mut point) = (0, 0, 0);
+    let dig_plans = parse(input, true);
+    shoelace_algorithm(dig_plans)
+}
 
-    let mut points = vec![(0, 0)];
+fn shoelace_algorithm(dig_plans: impl Iterator<Item = (char, isize)>) -> Result<isize> {
+    let mut start = (0, 0);
+    let mut area = 0;
+    for (direction, offset) in dig_plans {
+        let new_start = match direction {
+            'U' | '3' => (start.0 - offset, start.1),
+            'D' | '1' => (start.0 + offset, start.1),
+            'L' | '2' => (start.0, start.1 - offset),
+            'R' | '0' => (start.0, start.1 + offset),
+            _ => panic!("ERROR: bad input {direction}"),
+        };
+        area += (new_start.0 - start.0) * (start.1 + new_start.1) + offset;
+        start = new_start;
+    }
 
-    for (_, _, hex) in plan.digs {
-        let (offset, direction) = (
-            isize::from_str_radix(&hex[1..6], 16).unwrap(),
-            hex[6..].parse::<u8>().unwrap(),
-        );
-        match direction {
-            3 => {
-                r -= offset;
-            }
-            1 => {
-                r += offset;
-            }
-            2 => {
-                c -= offset;
-            }
-            0 => {
-                c += offset;
-            }
-            _ => panic!("ERROR: bad direction {direction}"),
+    Ok(area / 2 + 1)
+}
+
+fn parse(input: &str, hex_mode: bool) -> impl Iterator<Item = (char, isize)> + '_ {
+    input.lines().map(move |line| {
+        let mut line = line.split_ascii_whitespace();
+        let direction = line
+            .next()
+            .expect("ERROR: input should have move direction");
+        let offset = line
+            .next()
+            .expect("ERROR: input should have offset for move")
+            .parse::<isize>()
+            .unwrap();
+        let hex_data = line
+            .next()
+            .expect("ERROR: input should have color for a dig")
+            .trim_start_matches('(')
+            .trim_end_matches(')');
+
+        if hex_mode {
+            (
+                hex_data.chars().last().unwrap(),
+                isize::from_str_radix(&hex_data[1..6], 16).expect("ERROR: bad input"),
+            )
+        } else {
+            (direction.chars().next().unwrap(), offset)
         }
-        points.push((r, c));
-        point += offset;
-    }
-    let a = points
-        .iter()
-        .tuple_windows()
-        .map(|(p1, p2)| (p1.1 + p2.1) * (p2.0 - p1.0))
-        .sum::<isize>();
-    Ok((point + a) / 2 + 1)
-}
-
-struct Plan {
-    digs: Vec<(char, i32, String)>,
-}
-
-impl FromStr for Plan {
-    type Err = Box<dyn Error>;
-
-    fn from_str(input: &str) -> Result<Self> {
-        Ok(Self {
-            digs: input
-                .lines()
-                .map(|line| {
-                    let mut line = line.split_ascii_whitespace();
-                    let direction = line
-                        .next()
-                        .expect("ERROR: input should have move direction");
-                    let offset = line
-                        .next()
-                        .expect("ERROR: input should have offset for move")
-                        .parse::<i32>()
-                        .unwrap();
-                    let color = line
-                        .next()
-                        .expect("ERROR: input should have color for a dig")
-                        .trim_start_matches("(")
-                        .trim_end_matches(")");
-
-                    (
-                        direction.chars().next().unwrap(),
-                        offset,
-                        String::from(color),
-                    )
-                })
-                .collect_vec(),
-        })
-    }
+    })
 }
 
 #[cfg(test)]
@@ -137,6 +84,9 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(include_str!("../../inputs/day-18.txt")).unwrap(), 1);
+        assert_eq!(
+            part_2(include_str!("../../inputs/day-18.txt")).unwrap(),
+            60_612_092_439_765
+        );
     }
 }
