bf58fd7b75d97977a8452432f0d129bd5c7bdaed
triarupmac <tri-k.nguyen@arup.com>
2024-01-02 08:47:13 +1100

    update day-25 with stoer-wagner algorithm

--- src/bin/day-25.rs
+++ src/bin/day-25.rs
@@ -1,5 +1,7 @@
 use aoc_2023_lib::main;
 use itertools::Itertools;
+use priority_queue::PriorityQueue;
+use rustworkx_core::{connectivity::stoer_wagner_min_cut, petgraph::prelude::UnGraph};
 
 use std::{
     collections::{HashMap, HashSet},
@@ -13,97 +15,32 @@ main! {
     (part_1(input).unwrap(), '🎄')
 }
 
-fn component_size(graph: &HashMap<&str, HashSet<&str>>, a: &str) -> usize {
-    let mut seen = HashSet::new();
-    let mut s = vec![a];
-    while let Some(x) = s.pop() {
-        if !seen.insert(x) {
-            continue;
-        }
-        for b in &graph[x] {
-            if !seen.contains(b) {
-                s.push(b);
-            }
-        }
-    }
-    seen.len()
-}
-
 fn part_1(input: &str) -> Result<usize> {
-    let mut graph = HashMap::<_, HashSet<_>>::new();
-    let mut edges = HashSet::new();
-    for l in input.split('\n') {
-        let (a, rest) = l.split_once(": ").unwrap();
-        for b in rest.split(' ') {
-            graph.entry(a).or_default().insert(b);
-            graph.entry(b).or_default().insert(a);
-            let (x, y) = if a < b { (a, b) } else { (b, a) };
-            edges.insert((x, y));
+    let mut graph = rustworkx_core::petgraph::Graph::new_undirected();
+    let mut nodes = HashMap::new();
+    for line in input.lines() {
+        let mut parts = line.split(": ");
+        let node = parts.next().unwrap();
+        let node = *nodes.entry(node).or_insert_with(|| graph.add_node(node));
+        let edges = parts.next().unwrap().split(' ');
+        for edge in edges {
+            let edge = *nodes.entry(edge).or_insert_with(|| graph.add_node(edge));
+            graph.add_edge(node, edge, ());
         }
     }
-    let mut dot = String::from("graph {\n");
-    for (a, b) in &edges {
-        dot += &format!("{} -- {}\n", a, b);
-    }
-    dot += "}";
-    std::fs::write("out.dot", dot).unwrap();
-    // println!("Run the following to visualize the graph:");
-    // println!("dot -Tsvg -Kneato out.dot > out.svg");
-    // println!("Manually find the three edges.");
-    for (a, b) in [("xbl", "qqh"), ("tbq", "qfj"), ("xzn", "dsr")] {
-        graph.get_mut(a).unwrap().remove(b);
-        graph.get_mut(b).unwrap().remove(a);
-    }
-    let size = component_size(&graph, "qqh");
-    Ok((graph.len() - size) * size)
+    println!("rustworkx:");
+    let now = std::time::Instant::now();
+    let ans = match stoer_wagner_min_cut(&graph, |_| Ok::<i32, ()>(1)) {
+        Err(_) => unreachable!(),
+        Ok(None) => panic!("ERROR: no solution found"),
+        Ok(Some((cut, partition))) => {
+            println!("rustworkx-minmum-cut: {}", cut);
+            partition.len() * (nodes.len() - partition.len())
+        }
+    };
+    println!("p1-rustworkx: {} ({:?})", ans, now.elapsed());
+    Ok(ans)
 }
-// fn part_1(input: &str) -> Result<usize> {
-//     let graph = input.lines().fold(
-//         HashMap::new(),
-//         |mut acc: HashMap<&str, HashSet<&str>>, line| {
-//             let (lhs, rhs) = line.split_once(": ").unwrap();
-//             for c in rhs.split_ascii_whitespace() {
-//                 acc.entry(lhs).or_default().insert(c);
-//                 acc.entry(c).or_default().insert(lhs);
-//             }
-//             acc
-//         },
-//     );
-//     for (a, b, d, e, f, g) in graph.keys().cloned().tuple_combinations() {
-//         let mut new_graph = graph.clone();
-//         new_graph.get_mut(a).unwrap().remove(b);
-//         new_graph.get_mut(b).unwrap().remove(a);
-//         new_graph.get_mut(d).unwrap().remove(e);
-//         new_graph.get_mut(e).unwrap().remove(d);
-//         new_graph.get_mut(f).unwrap().remove(g);
-//         new_graph.get_mut(g).unwrap().remove(f);
-
-//         let mut ans = vec![HashSet::from([a]), HashSet::from([b])];
-
-//         let mut queue = Vec::new();
-//         queue.push(vec![a]);
-//         queue.push(vec![b]);
-
-//         for (index, mut group) in queue.into_iter().enumerate() {
-//             while let Some(element) = group.pop() {
-//                 ans[index].insert(element);
-
-//                 if let Some(others) = new_graph.get(element) {
-//                     for other in others {
-//                         if ans[index].insert(other) {
-//                             group.push(other);
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-
-//         if ans[0].len() + ans[1].len() == graph.keys().len() {
-//             return Ok(ans[0].len() * ans[1].len());
-//         }
-//     }
-//     unreachable!("ERROR: THERE MUST BE AN ANSWER")
-// }
 
 #[cfg(test)]
 mod tests {
